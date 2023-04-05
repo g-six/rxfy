e5781b06ca48fff1a7352c8cc0c2c4f3b3d250bf
g-six <gerard@echiverri.net>
2023-04-05 14:30:21 -0700

    feat: RxNotifications component plus types

--- src/_typings/events.ts
+++ src/_typings/events.ts
@@ -1,11 +1,22 @@
 export enum Events {
   Login = 'event-login',
   Loading = 'event-loading',
+  SystemNotification = 'system-notification',
   HomeAlertDismiss = 'event-home-alert-dismiss',
   HomeAlertSuccess = 'event-home-alert-success',
 }
 
+export enum NotificationCategory {
+  Error = 'error',
+  Success = 'success',
+  Warning = 'warning',
+  Info = 'info',
+}
+
 export interface EventsData {
+  clicked?: string;
   show?: boolean;
   time?: number;
+  message?: string;
+  category?: 'error' | 'success';
 }
