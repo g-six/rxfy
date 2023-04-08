eb08105cd4338f149222b22aa9fed21a702e18b7
g-six <gerard@echiverri.net>
2023-04-08 13:04:07 -0700

    feat: formatShortDate

--- src/_utilities/formatters.spec.ts
+++ src/_utilities/formatters.spec.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it } from 'vitest';
-import { formatPhone } from '@/_utilities/formatters';
+import { formatPhone, formatShortDate } from '@/_utilities/formatters';
 
 describe('formatPhone', () => {
   it('returns a properly formatted phone number', () => {
@@ -12,5 +12,19 @@ describe('formatPhone', () => {
 
   it('returns the input if unable to format it properly', () => {
     expect(formatPhone('123')).toBe('123');
+  });
+});
+
+describe('formatShortDate', () => {
+  it('returns a properly formatted date', () => {
+    expect(formatShortDate('03081983')).toBe('03/08/1983');
+  });
+
+  it('returns an empty string for an empty input', () => {
+    expect(formatShortDate('')).toBe('');
+  });
+
+  it('returns the best guess if input in different format', () => {
+    expect(formatShortDate('1-3-1994')).toBe('01/03/1994');
   });
 });
--- src/_utilities/formatters.ts
+++ src/_utilities/formatters.ts
@@ -12,3 +12,51 @@ export function formatPhone(input: string): string {
 
   return !grouped[2] ? grouped[1] : '(' + grouped[1] + ') ' + grouped[2] + (grouped[3] ? '-' + grouped[3] : '');
 }
+
+export function formatShortDate(input: string): string {
+  if (!input) return '';
+
+  // First, check separator
+  let cleaned: string = '';
+  if (input.indexOf('-') > 0) {
+    const portions = input.split('-');
+    if (portions.length < 3) {
+      return input;
+    }
+    portions.forEach((p, i) => {
+      if (i < 2) {
+        if (p.length > 2) return input;
+        if (p.length < 2) {
+          cleaned = `${cleaned}0${p}`;
+        } else {
+          cleaned = `${cleaned}${p}`;
+        }
+      }
+      if (i === 2) {
+        if (p.length > 4) return input;
+        if (p.length < 4) {
+          cleaned = `${cleaned}19${p.substring(0, 2)}`;
+        } else {
+          cleaned = `${cleaned}${p}`;
+        }
+      }
+    });
+  } else {
+    cleaned = input;
+  }
+
+  // Second, remove all non numeric values
+  cleaned = cleaned.replace(/\D/g, '');
+
+  if (cleaned.length < 8) return cleaned;
+
+  // Next, split into groups e.g. (day) (month) (last 4 digits as year)
+  const grouped: RegExpMatchArray | null = cleaned.match(/(\d{0,2})(\d{0,2})(\d{0,4})/);
+  if (grouped && grouped.length < 4) {
+    grouped.push('1999');
+  }
+  // If unable to format the input, just return as is
+  if (grouped === null) return input;
+
+  return !grouped[2] ? grouped[1] : grouped[1] + '/' + grouped[2] + (grouped[3] ? '/' + grouped[3] : '');
+}
