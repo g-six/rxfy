61538ecb55cc937e790bd60cf23f26eaf6c3e0c4
g-six <gerard@echiverri.net>
2023-04-12 17:31:53 -0700

    revert: shortdate formatter

--- src/_utilities/formatters.ts
+++ src/_utilities/formatters.ts
@@ -24,11 +24,49 @@ export function formatPhone(input: string): string {
 export function formatShortDate(input: string): string {
   if (!input) return '';
 
-  let day, month, year;
-  day = input.substring(0, 2);
-  month = input.substring(2, 4);
-  year = input.substring(4);
-  return `${day}/${month}/${year}`;
+  // First, check separator
+  let cleaned: string = '';
+  if (input.indexOf('-') > 0) {
+    const portions = input.split('-');
+    if (portions.length < 3) {
+      return input;
+    }
+    portions.forEach((p, i) => {
+      if (i < 2) {
+        if (p.length > 2) return input;
+        if (p.length < 2) {
+          cleaned = `${cleaned}0${p}`;
+        } else {
+          cleaned = `${cleaned}${p}`;
+        }
+      }
+      if (i === 2) {
+        if (p.length > 4) return input;
+        if (p.length < 4) {
+          cleaned = `${cleaned}19${p.substring(0, 2)}`;
+        } else {
+          cleaned = `${cleaned}${p}`;
+        }
+      }
+    });
+  } else {
+    cleaned = input;
+  }
+
+  // Second, remove all non numeric values
+  cleaned = cleaned.replace(/\D/g, '');
+
+  if (cleaned.length < 8) return cleaned;
+
+  // Next, split into groups e.g. (day) (month) (last 4 digits as year)
+  const grouped: RegExpMatchArray | null = cleaned.match(/(\d{0,2})(\d{0,2})(\d{0,4})/);
+  if (grouped && grouped.length < 4) {
+    grouped.push('1999');
+  }
+  // If unable to format the input, just return as is
+  if (grouped === null) return input;
+
+  return !grouped[2] ? grouped[1] : grouped[1] + '/' + grouped[2] + (grouped[3] ? '/' + grouped[3] : '');
 }
 
 export function validateEmail(email: string) {
