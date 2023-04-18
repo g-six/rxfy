3bbeb5859efdaf55e475bdf12ad7130d240e6980
g-six <gerard@echiverri.net>
2023-04-18 16:49:29 -0700

    fix: share contact card

--- src/components/A/RxDownloadLink.tsx
+++ src/components/A/RxDownloadLink.tsx
@@ -11,7 +11,6 @@ export default function RxDownloadLink({ children, agent, className, filename }:
   return (
     <a
       className={className}
-      download={filename}
       href={`data:text/vcard;charset=utf-8,BEGIN:VCARD\nVERSION:4.0\nN:${agent.full_name};;\nFN:${agent.full_name}\nORG:${
         agent.metatags?.brokerage_name || agent.full_name
       }\nTITLE:Leagent Realtor\nTEL;TYPE=WORK,VOICE:${agent.phone}\nEMAIL;TYPE=PREF,INTERNET:${agent.email}\nEND:VCARD`}
