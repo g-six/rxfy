1c9d3abc68e240f3f0baae6de1b7edb397214c49
g-six <gerard@echiverri.net>
2023-04-18 17:00:35 -0700

    fix notifications on navigator.share

--- src/components/RxShareButton.tsx
+++ src/components/RxShareButton.tsx
@@ -1,7 +1,5 @@
 'use client';
 import React from 'react';
-import useEvent from '@/hooks/useEvent';
-import { Events, NotificationCategory, NotificationMessages } from '@/_typings/events';
 
 type Props = {
   className: string;
@@ -9,7 +7,6 @@ type Props = {
   title: string;
 };
 export default function RxShareButton(p: Props) {
-  const { fireEvent } = useEvent(Events.SystemNotification);
   const copyLinkToClipboard = async () => {
     try {
       await navigator.share({
@@ -18,11 +15,7 @@ export default function RxShareButton(p: Props) {
         url: location.href,
       });
     } catch (err) {
-      fireEvent({
-        category: NotificationCategory.ERROR,
-        message: 'Sorry but your browser does not support sharing URLs, please copy the URL from the address bar to share instead.',
-        timeout: 4000,
-      });
+      console.log(err);
     }
   };
   return (
