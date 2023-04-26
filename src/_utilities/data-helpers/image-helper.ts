3ab66e1242e1f4135977bf9f3216a49d18549253
g-six <gerard@echiverri.net>
2023-04-26 15:24:34 -0700

    fix image-helper edge case

--- src/_utilities/data-helpers/image-helper.ts
+++ src/_utilities/data-helpers/image-helper.ts
@@ -5,5 +5,6 @@
  * @returns
  */
 export function getImageSized(url: string, px_size: number) {
+  if (url.indexOf('https://e52tn40a.cdn.imgeng.in') === 0) return url;
   return `https://e52tn40a.cdn.imgeng.in/w_${px_size}/${url}`;
 }
