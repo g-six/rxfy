e8077adc02d01a964830cccf380d144e87f726f6
g-six <gerard@echiverri.net>
2023-04-27 22:28:37 -0700

    fix rxcombo

--- src/components/RxCombobox.tsx
+++ src/components/RxCombobox.tsx
@@ -14,9 +14,9 @@ export default function RxCombobox(p: RxComboboxProps) {
 
   return (
     <div
-      className={`${p['data-value-for']} rexified ${p.className?.split('w-dropdown').join('')} ${styles.RxCombobox} ${opened ? 'w--open' : 'closed'}`}
-      onClick={() => {
-        toggleOpen(!opened);
+      className={`${p['data-value-for']} rexified ${p.className?.split('w-dropdown').join('')} ${styles.RxCombobox}`}
+      onClick={e => {
+        toggleOpen(true);
       }}
     >
       {p.children?.map((c: React.ReactElement) => {
@@ -24,16 +24,18 @@ export default function RxCombobox(p: RxComboboxProps) {
           ['aria-expanded']?: 'true' | 'false';
           children?: React.ReactElement;
         } = c.type === 'nav' ? {} : { ['aria-expanded']: opened ? 'true' : 'false' };
-        if (c.type === 'nav') {
+        if (c.type === 'div') {
           if (typeof c.props.children === 'object') {
             opts.children = <RxLiveCurrencyDD child={c.props.children} filter={p['data-value-for']} />;
           }
         }
         return React.cloneElement(c, {
-          className: `${c.props.className} rexified-${c.type} ${opened ? 'w--open' : 'closed'}`,
+          className: `${c.props.className} rexified-${c.type}`,
           ...opts,
-          onClick: () => {
-            toggleOpen(!opened);
+          onClick: (e: React.MouseEvent<HTMLAnchorElement>) => {
+            console.log(e.currentTarget.text);
+            e.preventDefault();
+            toggleOpen(false);
           },
         });
       })}
