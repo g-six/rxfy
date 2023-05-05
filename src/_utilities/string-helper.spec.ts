30bde878f5005c32da008f9d268561a49b9669dc
g-six <gerard@echiverri.net>
2023-05-05 16:48:44 -0700

    fix chatgpt's trivial mistake with one failing unit test

--- src/_utilities/string-helper.spec.ts
+++ src/_utilities/string-helper.spec.ts
@@ -1,8 +1,17 @@
-import { test, assert } from 'vitest';
-import { repeatChar } from './string-helper';
+import { describe, it, expect, test, assert } from 'vitest';
+import { emailToSlug, repeatChar } from './string-helper';
 
 test('should repeat a character for a specified number of times', () => {
   const expected = '*****';
   const actual = repeatChar('*', 5);
   assert.equal(actual, expected);
+});
+
+describe('emailToSlug', () => {
+  it('should convert a valid email address into a url friendly slug', () => {
+    const email = 'test@example.com';
+    const expected = 'test-example-com';
+    const result = emailToSlug(email);
+    expect(result).toEqual(expected);
+  });
 });
