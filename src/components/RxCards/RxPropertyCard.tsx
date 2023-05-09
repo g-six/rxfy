10c3822b2f3948fbc8f58bdf1fd778f748e5c06c
g-six <gerard@echiverri.net>
2023-05-08 19:25:59 -0700

    loader gifs

--- src/components/RxCards/RxPropertyCard.tsx
+++ src/components/RxCards/RxPropertyCard.tsx
@@ -131,21 +131,22 @@ function RxComponentChomper({ config, children }: any): any {
             }) as any,
           });
       } else if (RxElement.props.className === 'loading-animation') {
-        if (config.is_loading)
-          return React.cloneElement(child, {
-            ...RxElement.props,
-            className: RxElement.props.className,
-            children: (
-              <svg className='animate-spin -ml-1 mr-3 h-12 w-12 text-white' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
-                <circle className='opacity-50' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
-                <path
-                  className='opacity-100 text-indigo-600'
-                  fill='currentColor'
-                  d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                ></path>
-              </svg>
-            ),
-          });
+        return React.cloneElement(child, {
+          ...RxElement.props,
+          className: RxElement.props.className,
+          children: config.is_loading ? (
+            <svg className='animate-spin -ml-1 mr-3 h-12 w-12 text-white' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
+              <circle className='opacity-50' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+              <path
+                className='opacity-100 text-indigo-600'
+                fill='currentColor'
+                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+              ></path>
+            </svg>
+          ) : (
+            <></>
+          ),
+        });
       } else if (child.type !== 'img') {
         //heart-full
         if (RxElement.props.className === 'propcard-image') {
