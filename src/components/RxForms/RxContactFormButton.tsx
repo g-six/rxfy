ccbe07bd0b68345d9175ece198202007130d6f93
g-six <gerard@echiverri.net>
2023-05-09 07:51:19 -0700

    rexify contact button

--- src/components/RxForms/RxContactFormButton.tsx
+++ src/components/RxForms/RxContactFormButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function RxContactFormButton(p: { className: string; children: React.ReactElement[] }) {
   return (
-    <button className={p.className} type='button'>
+    <button className={[p.className, 'rexified'].join(' ').trim()} type='button'>
       {p.children}
     </button>
   );
