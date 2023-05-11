8354fc7f1d6c991698d29f465afe9087937a6512
g-six <gerard@echiverri.net>
2023-05-11 11:26:04 -0700

    PUT saved-searches

--- src/app/api/saved-searches/[id]/route.ts
+++ src/app/api/saved-searches/[id]/route.ts
@@ -8,6 +8,15 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
+const gql_update_search = `mutation UpdateSavedSearch($id: ID!, $updates: SavedSearchInput!) {
+    updateSavedSearch(id: $id, data: $updates) {
+        data {
+            id
+            attributes {${gqf_saved_search_attributes}}
+        }
+    }
+}`;
+
 const gql_delete_search = `mutation DeleteSavedSearch($id: ID!) {
     updateSavedSearch(id: $id, data: { customer: null }) {
         data {
@@ -56,6 +65,49 @@ export async function DELETE(request: Request) {
     }
   } catch (e) {
     error = 'Caught Bearer exception on DELETE method in \n  saved-searches/[id]/route.ts';
+    console.log(error);
+  }
+
+  return getResponse({ error, session_key, data }, error ? 400 : 200);
+}
+
+export async function PUT(request: Request) {
+  let session_key = '';
+  let error;
+  let data = {};
+  const { search_params } = await request.json();
+  try {
+    const { token, guid } = getTokenAndGuidFromSessionKey(request.headers.get('authorization') || '');
+    session_key = `${token}-${guid}`;
+
+    const id = Number(request.url.split('/').pop());
+    try {
+      const { data: update_response } = await axios.post(
+        `${process.env.NEXT_APP_CMS_GRAPHQL_URL}`,
+        {
+          query: gql_update_search,
+          variables: {
+            id,
+            updates: search_params,
+          },
+        },
+        {
+          headers,
+        },
+      );
+
+      data = {
+        ...data,
+        record: update_response.data.updateSavedSearch.data,
+      };
+    } catch (e) {
+      error = 'Caught exception on PUT method in \n  saved-searches/[id]/route.ts';
+      console.log(error);
+      const axios_error = e as AxiosError;
+      console.log(axios_error.response?.data);
+    }
+  } catch (e) {
+    error = 'Caught Bearer exception on PUT method in \n  saved-searches/[id]/route.ts';
     console.log(error);
   }
 
