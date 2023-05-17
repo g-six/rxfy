697ef4abe49ed594327081aeb06baa1465d07a46
g-six <gerard@echiverri.net>
2023-05-17 09:20:13 -0700

    homepage hydration error - fixing / debugging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,11 +116,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <body {...body_props} className={bodyClassName} suppressHydrationWarning>
             {children}
             {requestUrl.pathname === '/map' ? <Script src='https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.js' async /> : <></>}
-            <script type='text/javascript' async>
-              {initializePlacesAutocomplete({
-                apiKey: NEXT_APP_GGL_API_KEY || '',
-              })}
-            </script>
+            <script
+              type='text/javascript'
+              dangerouslySetInnerHTML={{
+                __html: initializePlacesAutocomplete({
+                  apiKey: NEXT_APP_GGL_API_KEY || '',
+                }),
+              }}
+            ></script>
             <script
               type='text/javascript'
               defer
