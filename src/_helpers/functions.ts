010db1f1ea3238accdf19f53f6d63dd4dfe8bcc6
g-six <gerard@echiverri.net>
2023-05-31 11:46:02 -0700

    Fix getIcon

--- src/_helpers/functions.ts
+++ src/_helpers/functions.ts
@@ -242,7 +242,7 @@ export function getFeatureIcons(property: unknown) {
   let features = {};
   const relationships = property as Record<string, string[]>;
   PROPERTY_ASSOCIATION_KEYS.forEach(relationship => {
-    if (relationships[relationship]) {
+    if (relationships[relationship] && Array.isArray(relationships[relationship])) {
       relationships[relationship].map(name => {
         features = {
           ...features,
