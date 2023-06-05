a9dad03b8ab03a9b2d4c3cc73e82acb82470c963
g-six <gerard@echiverri.net>
2023-06-05 11:43:32 -0700

    tweak AI results

--- src/app/api/agents/repair.ts
+++ src/app/api/agents/repair.ts
@@ -19,6 +19,16 @@ export async function getSmart(
   console.log('---');
   console.log('Processing:');
   // console.log(prompt);
+  console.log(`curl ${process.env.NEXT_APP_OPENAI_URI} -H 'content-type: application/json' -H 'authorization: bearer ${process.env.NEXT_APP_OPENAI_API}' \\`);
+  console.log(
+    ' -d',
+    JSON.stringify({
+      prompt,
+      max_tokens: 400,
+      temperature: 0.1,
+      model: 'text-davinci-003',
+    }),
+  );
   console.log('---');
   axios
     .post(
