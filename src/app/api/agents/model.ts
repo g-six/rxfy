34a3dd99bb972ae511c72639379f79f4f4db88ce
g-six <gerard@echiverri.net>
2023-06-06 17:08:12 -0700

    Additional data query for agent model

--- src/app/api/agents/model.ts
+++ src/app/api/agents/model.ts
@@ -4,6 +4,10 @@ import { WEBFLOW_THEME_DOMAINS } from '@/_typings/webflow';
 import { RealEstateBoardDataModel } from '@/_typings/real-estate-board';
 import { AgentInput } from '@/_typings/agent';
 import { getSmart } from './repair';
+import { retrieveFromLegacyPipeline } from '@/_utilities/api-calls/call-legacy-search';
+import { LegacySearchPayload } from '@/_typings/pipeline';
+import { getRealEstateBoard } from '../real-estate-boards/model';
+import { MLSProperty, PropertyDataModel } from '@/_typings/property';
 
 export async function createAgent(user_data: {
   agent_id: string;
@@ -66,6 +70,88 @@ export async function createAgent(user_data: {
   return {};
 }
 
+export async function findAgentRecordByAgentId(agent_id: string) {
+  const { data: response_data } = await axios.post(
+    `${process.env.NEXT_APP_CMS_GRAPHQL_URL}`,
+    {
+      query: gql_by_agent_id,
+      variables: {
+        agent_id,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_APP_CMS_API_KEY as string}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  let [agent] = response_data?.data?.agents.data;
+  if (!agent.attributes.agent_metatag?.data) {
+    const recent = await getMostRecentListing(agent_id);
+    const property = recent as { [key: string]: string | number };
+    const { real_estate_board } = recent as {
+      real_estate_board: {
+        id: number;
+        abbreviation: string;
+        name: string;
+      };
+    };
+    console.log({ property });
+    const ai_results = await getSmart(
+      {
+        agent_id,
+        full_name: agent.attributes.full_name,
+        email: agent.attributes.email,
+        id: agent.id,
+      },
+      property,
+      real_estate_board,
+    );
+    console.log({ ai_results });
+  }
+  return agent || null;
+}
+
+export async function getMostRecentListing(agent_id: string): Promise<unknown> {
+  const legacy_params: LegacySearchPayload = {
+    from: 0,
+    size: 1,
+    query: {
+      bool: {
+        should: [{ match: { 'data.LA1_LoginName': agent_id } }, { match: { 'data.LA2_LoginName': agent_id } }, { match: { 'data.LA3_LoginName': agent_id } }],
+        minimum_should_match: 0,
+      },
+    },
+  };
+  const legacy_listings = await retrieveFromLegacyPipeline(legacy_params, undefined, 1);
+  const listing = legacy_listings.length && legacy_listings[0];
+  if (listing) {
+    const {
+      title,
+      description,
+      lat,
+      lon,
+      area: target_area,
+      city: target_city,
+      asking_price,
+      property_type,
+      beds,
+      baths,
+      listed_at: listing_date,
+      ...mls_data
+    } = listing;
+    const legacy = mls_data as unknown as MLSProperty;
+    const real_estate_board = await getRealEstateBoard(mls_data as unknown as Record<string, string>);
+    let listed_by = legacy.LA1_FullName || legacy.LA2_FullName || legacy.LA3_FullName;
+    return {
+      ...listing,
+      listed_by,
+    };
+  }
+}
+
 export async function createAgentRecordIfNoneFound(
   { agent_id, email, phone, full_name }: AgentInput,
   real_estate_board?: RealEstateBoardDataModel,
@@ -89,26 +175,11 @@ export async function createAgentRecordIfNoneFound(
 
   try {
     // const variables = { email };
-    const variables = { agent_id };
-    const { data: response_data } = await axios.post(
-      `${process.env.NEXT_APP_CMS_GRAPHQL_URL}`,
-      {
-        query: gql_by_agent_id,
-        // query: gql_by_email,
-        variables,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_APP_CMS_API_KEY as string}`,
-          'Content-Type': 'application/json',
-        },
-      },
-    );
+    let agent = await findAgentRecordByAgentId(agent_id);
 
     let first_name = `${full_name}`.split(' ')[0];
     let last_name = `${full_name}`.split(' ').slice(0, 2).pop();
     last_name = (last_name && last_name.split('PREC*').join('').trim()) || '';
-    let [agent] = response_data?.data?.agents?.data;
 
     if (!agent) {
       console.log("Agent not found, let's create it");
