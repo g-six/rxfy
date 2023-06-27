881430beae85a3c38f8dd2b8ad14525f45a1d28e
g-six <gerard@echiverri.net>
2023-06-27 16:07:46 -0700

    multiple fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export function middleware(request: NextRequest) {
   // Store current request url in a custom header, which you can read later
   // we want to be able to read Property ID (MLS_ID, etc)
   // to place meta tags in HEAD dynamically based on Property Data
-  const { origin, pathname, searchParams } = new URL(request.url);
+  const { origin, pathname, searchParams, search } = new URL(request.url);
   const [, possible_agent_id, possible_slug, agent_page] = pathname.split('/');
   if (searchParams.get('key') && !request.cookies.get('session_key')) {
     response.cookies.set('session_key', searchParams.get('key') as string);
@@ -18,7 +18,7 @@ export function middleware(request: NextRequest) {
     response.cookies.set('agent_id', possible_agent_id);
     response.cookies.set('profile_slug', possible_slug);
     response.cookies.set('session_as', 'customer');
-    response.headers.set('x-url', `${origin}${agent_page ? `/${agent_page}` : ''}`);
+    response.headers.set('x-url', `${origin}${agent_page ? `/${agent_page}${search}` : search}`);
   } else {
     response.headers.set('x-url', request.url);
   }
