83fc9343091bdb8a29c9538968d87740ffb9da4c
Gerard Rey Echiverri <603961+g-six@users.noreply.github.com>
2023-06-29 09:00:44 -0700

    Set default homepage (#198)

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,13 +25,14 @@ export function middleware(request: NextRequest) {
   } else if (segments[0] === 'map') {
     response.headers.set('x-viewer', 'customer');
     page_url = `${page_url}${WEBFLOW_DASHBOARDS.CUSTOMER}/map`;
-  } else if (segments.length > 2 && segments[1].indexOf('la-') === 0) {
+  } else if (segments.length >= 2 && segments[1].indexOf('la-') === 0) {
     response.headers.set('x-agent-id', segments[0]);
     response.headers.set('x-profile-slug', segments[1]);
     response.headers.set('x-viewer', 'customer');
 
     if (segments[2] === 'map') page_url = `${page_url}${WEBFLOW_DASHBOARDS.CUSTOMER}/map`;
     else if (segments[2] === 'property') page_url = `${page_url}${WEBFLOW_DASHBOARDS.CUSTOMER}/property/propertyid`;
+    else page_url = `${page_url}${WEBFLOW_DASHBOARDS.CUSTOMER}`;
   } else if (pathname === '/') {
     page_url = `${page_url}${WEBFLOW_DASHBOARDS.REALTOR}`;
   } else {
