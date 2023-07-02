85793d05f96ca31befc76e1214e43567f8c79706
rsemenyshyn <ripka.ua@gmail.com>
2023-07-02 22:56:47 +0200

    fixed tool architecture

--- src/components/RxTools/RxPropertyBrochures.tsx
+++ src/components/RxTools/RxPropertyBrochures.tsx
@@ -4,13 +4,13 @@ import { ReplacerPageProps } from '@/_typings/forms';
 import { tMatch, transformMatchingElements } from '@/_helpers/dom-manipulators';
 import { searchByClasses } from '@/_utilities/rx-element-extractor';
 
-export default function RxPropertyBrochures({ nodes }: ReplacerPageProps) {
+export default function RxPropertyBrochures({ nodes, agent }: ReplacerPageProps) {
   const matches: tMatch[] = [
     {
       searchFn: searchByClasses(['go-to-map']),
       transformChild: (child: React.ReactElement) => {
         return React.cloneElement(child, {
-          href: '/map',
+          href: (agent.webflow_domain ? agent.webflow_domain : '') + '/map',
         });
       },
     },
