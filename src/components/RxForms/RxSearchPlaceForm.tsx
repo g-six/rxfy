8cc23ebf5893556ca279877ffe6ba2082495d7a1
g-six <gerard@echiverri.net>
2023-07-03 13:11:28 -0700

    Fix SearchAddress form in agent home page

--- src/components/RxForms/RxSearchPlaceForm.tsx
+++ src/components/RxForms/RxSearchPlaceForm.tsx
@@ -1,9 +1,23 @@
+'use client';
+import SearchAddressCombobox from '@/_replacers/FilterFields/SearchAddressCombobox';
+import { objectToQueryString } from '@/_utilities/url-helper';
 import React from 'react';
 
 type Props = {
   children: React.ReactElement;
   className?: string;
 };
+
+const onPlaceSelected = (p: { [key: string]: string }) => {
+  const qry = objectToQueryString(p);
+  const u = new URL(location.href);
+  if (u.pathname.split('/').length > 2) {
+    location.href = `${location.href}/map?${qry}`;
+  } else {
+    location.href = `/map?${qry}`;
+  }
+};
+
 function Iterator(p: Props & { id?: string; 'data-session': unknown; onClick: React.MouseEventHandler }) {
   const collection = React.Children.map(p.children, child => {
     if (!child.props) {
@@ -12,6 +26,8 @@ function Iterator(p: Props & { id?: string; 'data-session': unknown; onClick: Re
       return React.cloneElement(child, {
         className: `${child?.props?.className || ''} rexified`.trim(),
       });
+    } else if (child.type === 'input') {
+      return <SearchAddressCombobox {...child.props} onPlaceSelected={onPlaceSelected} />;
     } else if (child.type && !['div', 'form'].includes(child.type as string)) {
       return child;
     }
