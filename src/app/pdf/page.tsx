a2f72bc940dfd6bcc40e5ee441143c5ddf6f3c90
g-six <gerard@echiverri.net>
2023-07-07 08:17:40 -0700

    fix pdf page

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { getPdf } from '@/app/api/_helpers/pdf-helper';
 import { getPropertyByMlsId } from '@/app/api/properties/model';
 import { findAgentRecordByAgentId } from '@/app/api/agents/model';
-import html2canvas from 'html2canvas';
-import { domToReact, htmlToDOM } from 'html-react-parser';
 
 export default async function PdfPage({ searchParams }: any) {
-  let pdf = '';
   if (searchParams.agent && searchParams.slug && searchParams.mls) {
     const agent = await findAgentRecordByAgentId(searchParams.agent as string);
     const data = await getPropertyByMlsId(searchParams.mls as string);
@@ -16,11 +13,12 @@ export default async function PdfPage({ searchParams }: any) {
       agent,
     } as unknown;
     const page_url = ['https://', process.env.NEXT_PUBLIC_DEFAULT_THEME_DOMAIN, 'brochure'].join('/');
-    pdf = await getPdf(page_url, full_data as unknown);
+    const buff = await getPdf(page_url, full_data as unknown);
+    const blob = new Blob([buff], { type: 'application/pdf' });
+    return <>{blob}</>;
   } else {
     console.log(searchParams, searchParams);
   }
 
-  const dom = htmlToDOM(pdf);
-  return domToReact(dom);
+  return <></>;
 }
