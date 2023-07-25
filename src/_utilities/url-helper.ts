9b55f8294bd6f31b27830ea0549db689a89b247d
g-six <gerard@echiverri.net>
2023-07-25 12:08:59 -0700

    fix url-helper - query string blank

--- src/_utilities/url-helper.ts
+++ src/_utilities/url-helper.ts
@@ -2,10 +2,12 @@ export function queryStringToObject(queryString: string): Record<string, string 
   const queryPairs = queryString.split('&');
   const queryObject: Record<string, string | number> = {};
 
-  queryPairs.forEach(pair => {
-    const [key, value] = pair.split('=');
-    queryObject[key] = isNaN(Number(value)) ? decodeURIComponent((value || '').replace(/\+/g, ' ')) : Number(value);
-  });
+  queryPairs
+    .filter(pair => pair)
+    .forEach(pair => {
+      const [key, value] = pair.split('=');
+      queryObject[key] = isNaN(Number(value)) ? decodeURIComponent((value || '').replace(/\+/g, ' ')) : Number(value);
+    });
 
   return queryObject;
 }
