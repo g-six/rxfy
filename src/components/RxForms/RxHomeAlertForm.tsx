74fdf3fb2160e8596c6d14be2ce8c05dc89f2de7
g-six <gerard@echiverri.net>
2023-08-02 07:30:02 -0700

    home alerts POST api

--- src/components/RxForms/RxHomeAlertForm.tsx
+++ src/components/RxForms/RxHomeAlertForm.tsx
@@ -560,7 +560,7 @@ export default function RxHomeAlertForm(p: Props) {
             };
 
             if (listed_at) {
-              search_params.listed_at = new Date(listed_at).toISOString();
+              search_params.add_date = new Date(listed_at).toISOString().split('T')[0];
             }
 
             alertData?.id
