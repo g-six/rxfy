a7bd4d3073af4e76366b985131b398aa75101d61
g-six <gerard@echiverri.net>
2023-08-02 08:03:44 -0700

    session check

--- src/app/[slug]/[profile-slug]/my-home-alerts/page.tsx
+++ src/app/[slug]/[profile-slug]/my-home-alerts/page.tsx
@@ -7,11 +7,24 @@ import { CheerioAPI, load } from 'cheerio';
 import Container from './container.module';
 import { AgentData } from '@/_typings/agent';
 import { getImageSized } from '@/_utilities/data-helpers/image-helper';
+import { getUserSessionData } from '@/app/api/check-session/route';
+import { cookies, headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default async function MyHomeAlerts({ params }: { params: { [key: string]: string } }) {
-  const promises = await Promise.all([axios.get('https://' + WEBFLOW_DASHBOARDS.CUSTOMER + '/my-home-alerts'), findAgentRecordByAgentId(params.slug)]);
+  if (!cookies().get('session_key')?.value) redirect(`log-in`);
+
+  const promises = await Promise.all([
+    axios.get('https://' + WEBFLOW_DASHBOARDS.CUSTOMER + '/my-home-alerts'),
+    findAgentRecordByAgentId(params.slug),
+    getUserSessionData(`Bearer ${cookies().get('session_key')?.value}`, 'customer'),
+  ]);
   const { data: html } = promises[0];
   const agent: AgentData = promises[1];
+  const user = promises[2];
+
+  if (!agent?.agent_id) redirect(`/log-in`);
+  if (!user) redirect(`/${agent.agent_id}/${agent.metatags.profile_slug}/log-in`);
 
   if (html) {
     const $: CheerioAPI = load(html);
