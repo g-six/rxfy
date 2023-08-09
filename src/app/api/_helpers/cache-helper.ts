8075b49e40f6831c727bfdd2a4b528b2040bfd98
Gerard Rey <gerard@echiverri.net>
2023-08-09 09:30:44 -0700

    Error handling in invalidateCache

--- src/app/api/_helpers/cache-helper.ts
+++ src/app/api/_helpers/cache-helper.ts
@@ -2,27 +2,32 @@ import { CloudFrontClient, CreateInvalidationCommand } from '@aws-sdk/client-clo
 import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
 
 export function invalidateCache(Items: string[]) {
-  const NEXT_APP_SITES_DIST_ID = process.env.NEXT_APP_SITES_DIST_ID as string;
-  const client = new CloudFrontClient({
-    region: 'us-west-2',
-    credentials: {
-      accessKeyId: process.env.NEXT_APP_UPLOADER_KEY_ID as string,
-      secretAccessKey: process.env.NEXT_APP_UPLOAD_SECRET_KEY as string,
-    },
-  });
-  const command = new CreateInvalidationCommand({
-    DistributionId: NEXT_APP_SITES_DIST_ID,
-    InvalidationBatch: {
-      CallerReference: new Date().getTime().toString(),
-      Paths: {
-        Quantity: Items.length,
-        Items,
+  try {
+    const NEXT_APP_SITES_DIST_ID = process.env.NEXT_APP_SITES_DIST_ID as string;
+    const client = new CloudFrontClient({
+      region: 'us-west-2',
+      credentials: {
+        accessKeyId: process.env.NEXT_APP_UPLOADER_KEY_ID as string,
+        secretAccessKey: process.env.NEXT_APP_UPLOAD_SECRET_KEY as string,
       },
-    },
-  });
-  console.log('Invalidating cache for ', NEXT_APP_SITES_DIST_ID);
-  console.log(JSON.stringify(Items, null, 4));
-  return client.send(command);
+    });
+    const command = new CreateInvalidationCommand({
+      DistributionId: NEXT_APP_SITES_DIST_ID,
+      InvalidationBatch: {
+        CallerReference: new Date().getTime().toString(),
+        Paths: {
+          Quantity: Items.length,
+          Items,
+        },
+      },
+    });
+    console.log('Invalidating cache for ', NEXT_APP_SITES_DIST_ID);
+    console.log(JSON.stringify(Items, null, 4));
+    return client.send(command);
+  } catch (e) {
+    console.error('Error in function invalidateCache');
+    console.log(JSON.stringify({ Items }, null, 4));
+  }
 }
 
 export function createCacheItem(Body: any, Key: string, ContentType: string = 'text/json') {
