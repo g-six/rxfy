01cecb82a2e9dd25d67a7a4ae579106bb2d4e48b
Gerard Rey <gerard@echiverri.net>
2023-09-01 10:33:00 -0700

    Property page

--- src/app/property/features.iterator.tsx
+++ src/app/property/features.iterator.tsx
@@ -18,20 +18,25 @@ export default function IconIterator({ children, property, className }: { childr
 
   iconables.forEach(({ name }, idx) => {
     const key = slugifyAddress(name.toLowerCase());
+    const alias = getAlias(key);
+    if (alias === 'water') {
+      if (!has_water) has_water = true;
+      else return;
+    }
     if (!no_icons.includes(key))
       Icons.push(
         <div key={`${idx}-${key}`} className={className}>
           {Children.map(children, c => {
             let icons: ReactElement[] = [];
             if (c.type === 'img') {
-              return cloneElement(c, { src: `/icons/features/feature_${getAlias(key)}.svg` });
+              return cloneElement(c, { src: `/icons/features/feature_${alias}.svg` });
             }
             return cloneElement(
               c,
               {
                 key: key,
               },
-              name,
+              alias === 'water' ? 'Public Water Supply' : name,
             );
           })}
         </div>,
@@ -42,7 +47,7 @@ export default function IconIterator({ children, property, className }: { childr
 }
 
 function getAlias(feature: string) {
-  if (feature.includes('water')) return 'water';
+  if (feature.includes('water-supply')) return 'water';
   switch (feature) {
     case 'city-town-centre':
       return 'city-municipal';
