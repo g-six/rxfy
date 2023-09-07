5b5cc9940f5ab7380f37c4516ba2d2bc9e78751b
Gerard Rey <gerard@echiverri.net>
2023-09-07 10:54:59 -0700

    deply navbar

--- src/app/navbar.module.tsx
+++ src/app/navbar.module.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { convertDivsToSpans } from '@/_replacers/DivToSpan';
 import { AgentData } from '@/_typings/agent';
+import { Menu, Transition } from '@headlessui/react';
 import { slugifyAddress } from '@/_utilities/data-helpers/property-page';
 import { classNames } from '@/_utilities/html-helper';
 import { objectToQueryString } from '@/_utilities/url-helper';
 import { useRouter } from 'next/navigation';
 import { Children, ReactElement, SyntheticEvent, cloneElement } from 'react';
+import SimpleDropdown from '@/components/Dropdowns/SimpleDropdown.module';
 
 function Iterator({ children, handleEvent }: { children: ReactElement; handleEvent(event_name: string): void }) {
   const Rexified = Children.map(children, c => {
@@ -21,29 +23,21 @@ function Iterator({ children, handleEvent }: { children: ReactElement; handleEve
         });
     }
     if (c.type === 'a') {
-      return cloneElement(
-        <div
-          onClick={(evt: SyntheticEvent<HTMLDivElement>) => {
-            handleEvent(slugifyAddress(evt.currentTarget.innerText));
-          }}
-        />,
-        {
-          ...c.props,
-          href: undefined,
-          className: classNames(c.props.className || '', 'cursor-pointer'),
-        },
-        convertDivsToSpans(c.props.children),
-      );
+      return cloneElement(c, {}, convertDivsToSpans(c.props.children));
     }
     if (['svg', 'img'].includes(c.type as string)) {
       return c;
     }
     if (c.props?.children) {
+      let className = classNames(c.props.className || '', 'navbar.module.Iterator.' + c.type);
+      if (className.includes('w-dropdown')) {
+        return <SimpleDropdown className={className}>{c.props.children}</SimpleDropdown>;
+      }
       return cloneElement(
-        c.type === 'a' ? <a className={c.props.className || ''} /> : <div className={c.props.className || ''} />,
+        c.type === 'a' ? <a /> : <div />,
         {
           ...kv,
-          href: '/ddd',
+          className,
         },
         typeof c.props.children === 'string' ? c.props.children : <Iterator handleEvent={handleEvent}>{c.props.children}</Iterator>,
       );
@@ -63,7 +57,7 @@ export default function Navbar({ agent, children }: { agent?: AgentData; childre
         map_url = `${agent.metatags.profile_slug}${map_url}`;
       }
       router.push(map_url);
-    }
+    } else console.log({ evt_name });
   };
   return (
     <nav>
