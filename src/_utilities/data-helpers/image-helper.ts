b0e211600745c5f0f741860f0f95606a9678468e
Gerard Rey <gerard@echiverri.net>
2023-09-07 15:32:56 -0700

    fix map price

--- src/_utilities/data-helpers/image-helper.ts
+++ src/_utilities/data-helpers/image-helper.ts
@@ -5,6 +5,7 @@
  * @returns
  */
 export function getImageSized(url: string, px_size: number = 999) {
+  if (!url) return url;
   if (url.includes('blob')) return url;
   if (url.indexOf(`${process.env.NEXT_PUBLIC_IM_ENG}`) === 0) return url;
   return `${process.env.NEXT_PUBLIC_IM_ENG}/w_${px_size}/${url}`;
--- src/_utilities/data-helpers/price-helper.ts
+++ src/_utilities/data-helpers/price-helper.ts
@@ -4,11 +4,11 @@ export function getShortPrice(amount: number, prefix = '$') {
     return `${prefix}${str}K`;
   }
 
-  if (str.substring(1, 2) !== '0') {
-    const x = Math.round(parseInt(str.substring(1), 10) / 100);
-    if (x < 10) return `${prefix}${str.substring(0, 1)}.${x}M`;
-    else return `${prefix}${str.substring(0, 1)}M`;
-  }
+  // if (str.substring(1, 2) !== '0') {
+  //   const x = Math.round(parseInt(str.substring(1), 10) / 100);
+  //   if (x < 10) return `${prefix}${str.substring(0, 1)}.${x}M`;
+  //   else return `${prefix}${str.substring(0, 1)}M`;
+  // }
 
-  return `${prefix}${Math.round(amount / 1000000)}M`;
+  return `${prefix}${Math.round(amount / 100000) / 10}M`;
 }
