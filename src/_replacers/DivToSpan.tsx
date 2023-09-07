72d119ff4cdd9cdf26f81c8f268c843abdd86050
Gerard Rey <gerard@echiverri.net>
2023-09-07 15:36:28 -0700

    fix convertDivsToSpan svg bug

--- src/_replacers/DivToSpan.tsx
+++ src/_replacers/DivToSpan.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function convertDivsToSpans(el: React.ReactElement) {
-  if (el.props?.children && el.type !== 'span')
+  if (el.props?.children && el.type !== 'span' && el.type !== 'svg')
     return (
       <span {...el.props} className={el.props?.className || '' + ' rexified converted-div'}>
         {React.Children.map(el.props.children, convertDivsToSpans)}
