5404da0475ae84ea482c822b43cb1c381697b175
Gerard Rey <gerard@echiverri.net>
2023-10-20 13:29:23 -0700

    debug stripe changes

--- src/app/api/check-session/agent/route.ts
+++ src/app/api/check-session/agent/route.ts
@@ -1,4 +1,22 @@
-import { GET as checkSessionRoute } from '../route';
+import { AxiosError } from 'axios';
+import { getResponse } from '../../response-helper';
+import { getUserSessionData } from '../model';
+
 export async function GET(request: Request) {
-  return await checkSessionRoute(request);
+  let results = {};
+  try {
+    results = await getUserSessionData(request.headers.get('authorization') || '', 'realtor');
+    const { error } = results as unknown as { error: string };
+    if (error) return getResponse(results, 401);
+    return getResponse(results, 200);
+  } catch (e) {
+    const { response } = e as AxiosError;
+    if (response?.data) {
+      const { errors } = response?.data as unknown as {
+        errors?: unknown[];
+      };
+      return getResponse({ error: 'api.check-session.GET error.  See server logs for details' }, 400);
+    }
+    return getResponse({ e, error: 'api.check-session.GET error.  See server logs for details' }, 400);
+  }
 }
