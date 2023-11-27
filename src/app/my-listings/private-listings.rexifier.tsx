b10703432ff39c7a16cb71483d5ac63496543e8f
Gerard Rey <gerard@echiverri.net>
2023-11-27 15:55:18 -0800

    Bugfix: page not redirecting after creation

--- src/app/my-listings/private-listings.rexifier.tsx
+++ src/app/my-listings/private-listings.rexifier.tsx
@@ -42,7 +42,7 @@ export default async function MyListingsPrivateListings({ children, agent, ...pr
               <></>
             ),
           )
-        : '<!-- No listings -->'}
+        : ''}
     </section>
   );
 }
