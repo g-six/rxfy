439f5b539d553c166f75f19445de56bd52c55829
Gerard Rey <gerard@echiverri.net>
2023-12-21 13:12:08 -0800

    use fetch instead of axios for graphQL

--- src/app/api/_helpers/graphql-helper.ts
+++ src/app/api/_helpers/graphql-helper.ts
@@ -1,23 +1,28 @@
-import axios from 'axios';
-
 export async function graphQL(query: string, variables: Record<string, unknown>) {
   try {
-    const {
-      data: { data },
-    } = await axios.post(
-      `${process.env.NEXT_APP_CMS_GRAPHQL_URL}`,
-      {
+    const response = await fetch(`${process.env.NEXT_APP_CMS_GRAPHQL_URL}`, {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_APP_CMS_API_KEY as string}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
         query,
         variables,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_APP_CMS_API_KEY as string}`,
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-    return data;
+      }),
+    });
+    if (response.ok) {
+      const json = await response.json();
+      return json.data;
+    } else {
+      console.error('Error in graphQL');
+      console.error('');
+      console.error(query);
+      console.error('');
+      console.error('');
+      console.error({ variables });
+      return;
+    }
   } catch (e) {
     console.error('Error in graphQL');
     console.error('');
